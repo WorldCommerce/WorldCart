e8a024c7eea93f7953981ed63c179350b5ea6485
jackvsworld <ajbarton@users.sf.net>
2015-04-20 21:30:12 -0700

    Add WCF project template

---
+++ src/Services/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using WorldCart.Data;
+
+namespace WorldCart.Services
+{
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+  public class Service1 : IService1
+  {
+    public Order GetData(int value)
+    {
+      using (var db = new CheckoutEntities())
+      {
+        return new Order { ID = value };
+        //return db.WorldCart_Orders.SingleOrDefault(o => o.ID == value);
+      }
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    {
+      if (composite == null)
+      {
+        throw new ArgumentNullException("composite");
+      }
+      if (composite.BoolValue)
+      {
+        composite.StringValue += "Suffix";
+      }
+      return composite;
+    }
+  }
+}
