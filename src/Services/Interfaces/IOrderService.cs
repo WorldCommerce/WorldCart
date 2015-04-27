1c196aa2965c716bf0aa8a0dd3365130990e935a
jackvsworld <ajbarton@users.sf.net>
2015-04-27 01:03:54 -0700

    Create WCF OrderService
    Squashed commit of the following:
    
    commit b7aef370566eebace8680f71aa1b535ab46a4cec
    Author: jackvsworld <ajbarton@users.sf.net>
    Date:   Mon Apr 27 01:02:02 2015 -0700
    
        Create WCF OrderService
    
    commit cdf2706fe517034ad5f02b01cf82037d15d02c51
    Merge: 2aa975c ad8d4db
    Author: jackvsworld <ajbarton@users.sf.net>
    Date:   Mon Apr 27 00:56:25 2015 -0700
    
        Merge branch 'dev' into wcf
    
    commit 2aa975cce019835a853ac986033d34ec03d85a72
    Author: jackvsworld <ajbarton@users.sf.net>
    Date:   Mon Apr 27 00:37:28 2015 -0700
    
        Add GetOrder() method

---
+++ src/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using WorldCart.Data;
+
+namespace WorldCart.Services.Interfaces
+{
+  [ServiceContract]
+  public interface IOrderService
+  {
+    [OperationContract]
+    Order GetOrder(string orderNumber);
+
+    // TODO: Add your service operations here
+  }
+}
---
+++ src/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using WorldCart.Data;
+using WorldCart.Services.Interfaces;
+
+namespace WorldCart.Services
+{
+  public class OrderService : IOrderService
+  {
+    public Order GetOrder(string orderNumber)
+    {
+      using (var db = new WorldCartEntities())
+      {
+        return db.Orders.Single(o => o.OrderNumber == orderNumber);
+      }
+    }
+  }
+}
