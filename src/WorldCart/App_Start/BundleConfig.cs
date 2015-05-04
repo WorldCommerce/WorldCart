b97dbb293407ebcb84ba5ccd8ec51380caaa88b2
jackvsworld <ajbarton@users.sf.net>
2015-05-04 02:09:56 -0700

    Add AngularJS and Bootstrap
    Squashed commit of the following:
    
    commit c8f4f05a449f7994ff49b777cddc9a60485ee642
    Author: jackvsworld <ajbarton@users.sf.net>
    Date:   Mon May 4 02:05:42 2015 -0700
    
        Add references for AngularJS and Bootstrap
    
    commit fc12360b2b903b11fd939bef93ef7b030f1f7ac2
    Author: jackvsworld <ajbarton@users.sf.net>
    Date:   Mon May 4 02:04:02 2015 -0700
    
        Add AngularJS and Bootstrap

--- src/WorldCart/App_Start/BundleConfig.cs
+++ src/WorldCart/App_Start/BundleConfig.cs
@@ -8,15 +8,21 @@ namespace WorldCart
     // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
     public static void RegisterBundles(BundleCollection bundles)
     {
-      bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-{version}.js"));
+      bundles.Add(new ScriptBundle("~/bundles/jquery")
+        .Include("~/Scripts/jquery-{version}.js"));
+
+      bundles.Add(new ScriptBundle("~/bundles/angular")
+        .Include("~/Scripts/angular.js")
+        .Include("~/Scripts/angular-resource.js"));
 
       // Use the development version of Modernizr to develop with and learn from. Then, when you're
       // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-      bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                  "~/Scripts/modernizr-*"));
+      bundles.Add(new ScriptBundle("~/bundles/modernizr")
+        .Include("~/Scripts/modernizr-*"));
 
-      bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+      bundles.Add(new StyleBundle("~/Content/css")
+        .Include("~/Content/bootstrap.css")
+        .Include("~/Content/site.css"));
     }
   }
 }
