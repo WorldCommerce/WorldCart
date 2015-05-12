b78ca318b5385d47cf0e3fb340c0e46e259bae55
jackvsworld <ajbarton@users.sf.net>
2015-05-12 00:21:31 -0700

    Edit Index.cshtml
    Use a model with the Razor view.

--- src/WorldCart/Controllers/HomeController.cs
+++ src/WorldCart/Controllers/HomeController.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using WorldCart.Models;
+
 namespace WorldCart.Controllers
 {
   public class HomeController : Controller
   {
     public ActionResult Index()
     {
-      return View();
+      return View("Index", new Model1 { Title = "Model1", Summary = "Hello, world!" });
     }
   }
 }
